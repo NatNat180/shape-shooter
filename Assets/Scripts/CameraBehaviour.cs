ad60b74165f3e38625fe0002ce5685b1ddbdf956
NatNat180 <Nathan.Argall@gmail.com>
2018-01-08 11:04:50 -0600

    take out shake behaviour

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -4,17 +4,11 @@ using UnityEngine;
 
 public class CameraBehaviour : MonoBehaviour
 {
-    float shakeDuration;
-    float shakeMagnitude;
-	Vector3 originalCamPos;
 	Renderer lensRenderer;
 	Color lensColor;
 
     void Start()
     {
-		originalCamPos = Camera.main.transform.position;
-		shakeDuration = 0.1f;
-		shakeMagnitude = 0.5f;
 		lensRenderer = GetComponentInChildren<Renderer>();
 		// set initial alpha color of lens to 0
 		lensColor = lensRenderer.material.color;
@@ -26,36 +20,8 @@ public class CameraBehaviour : MonoBehaviour
     {
 		if (GameProperties.PlayerHurtSound.isPlaying && Time.timeScale != 0)
 		{
-			StartCoroutine(Shake());
 			StartCoroutine(Redden());
 		}
-    }
-
-    IEnumerator Shake()
-    {
-        float elapsed = 0.0f;
-
-        while (elapsed < shakeDuration)
-        {
-            elapsed += Time.deltaTime;
-
-            float percentComplete = elapsed / shakeDuration;
-			// 'dampen' shake animation as shake animation nears 100 percent completion
-            float damper = 1.0f - Mathf.Clamp(5.0f * percentComplete - 3.0f, 0.0f, 1.0f);
-
-            // create random x and y offsets from original cam position
-            float x = Random.value * 2.0f - 1.0f;
-            float y = Random.value * 2.0f - 1.0f;
-            x *= shakeMagnitude * damper;
-            y *= shakeMagnitude * damper;
-				
-            Camera.main.transform.position = new Vector3(x, y, originalCamPos.z);
-
-            yield return null;
-        }
-
-        Camera.main.transform.position = originalCamPos;
-
     }
 
 	IEnumerator Redden()
